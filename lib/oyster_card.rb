5f3577913c14aefb665742ee3251613113daffa7
benfurber <ben@furber-consultancy.com>
2018-04-23 17:53:55 +0100

    Test/feature to deduct fare amount from balance

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -6,7 +6,11 @@ class Oystercard
   end
 
   def top_up(number)
-    raise 'Sorry, total balance would exceed £90' if (@balance + number) >= 90
+    raise "Sorry, #{balance + number} would exceed maximium of £90" if (@balance + number) >= 90
     @balance += number
+  end
+
+  def deduct(fare)
+    @balance -= fare
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,13 +6,18 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
-  context 'An oyster card' do
+  context 'Any oyster card' do
     it 'Adds top up value to existing balance' do
       subject.top_up(50)
       expect(subject.balance).to eq 50
     end
     it 'Allows a maximum total balance of £90' do
       expect { subject.top_up(91) }.to raise_error(RuntimeError)
+    end
+    it 'Deducting fare amount from balance' do
+      subject.top_up(10)
+      subject.deduct(5)
+      expect(subject.balance).to eq 5
     end
   end
 end
