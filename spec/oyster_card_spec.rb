66df95d36b46a4f8c7a908d1a7646947f171b0f4
benfurber <ben@furber-consultancy.com>
2018-04-23 16:59:05 +0100

    Test/feature for topping up oystercard

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -2,8 +2,14 @@ require 'oyster_card'
 
 describe Oystercard do
   context 'A new oyster card' do
-    it 'with a default balance of zero' do
+    it 'Has a default balance of zero' do
       expect(subject.balance).to eq 0
+    end
+  end
+  context 'An oyster card' do
+    it 'Adds top up value to existing balance' do
+      subject.top_up(50)
+      expect(subject.balance).to eq 50
     end
   end
 end
