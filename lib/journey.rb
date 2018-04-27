3fd73db2cd0615399f84c72df1bc39b012ed1428
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-27 16:04:45 +0100

    18 passing tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,8 +1,27 @@
 class Journey
 
-def history
-history = []
+attr_reader :history, :journey
+
+def initialize
+  @history = []
+  @journey = {}
+  @complete = true
 end
 
+def adds_journey
+@history << @journey
+end
+
+def complete?
+  @complete
+end
+
+def start
+  @complete = false
+end
+
+def end
+  @complete = true
+end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -8,4 +8,43 @@ describe '#history', :history do
     expect(subject.history).to eq []
     end
   end
+
+describe '#journey', :journey do
+  it 'Shows empty journey hash on initialization' do
+    expect(subject.journey).to be_empty
+    end
+  # it 'Tests elements in journey hash' do
+  #   subject.complete?
+  #   expect(subject.journey.length).to eq 2
+  # end
+end
+
+  describe '#complete?', :complete do
+    it 'Shows status of complete?' do
+    expect(subject.complete?).to eq true
+    end
+  end
+
+describe '#adds_journey', :adds_journey do
+  it 'Adds journey to history' do
+    subject.adds_journey
+    expect(subject.history.length).to eq 1
+    end
+  end
+
+describe '#start', :start do
+  it 'Changes complete? to false' do
+    subject.start
+    expect(subject.complete?).to eq false
+  end
+end
+
+describe '#end', :end do
+  it 'Changes complete? to true' do
+    subject.start
+    subject.end
+    expect(subject.complete?).to eq true
+    end
+end
+
 end
